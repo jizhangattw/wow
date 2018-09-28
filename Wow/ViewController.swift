b3a4faa330abd5fe819a2689fd61088714e422bc
Jian Zhang <jizhang@thoughtworks.com>
2018-09-28 21:17:47 +0800

    init

---
+++ Wow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wow
+//
+//  Created by Jian Zhang  on 2018/9/28.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
